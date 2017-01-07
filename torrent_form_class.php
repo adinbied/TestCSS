8d4a214454e4300c86c88c4c0afc364befc70323
adinbied <adinbied@gmail.com>
2017-01-06 22:40:58 -0800

    Update torrent_form_class.php

--- torrent_form_class.php
+++ torrent_form_class.php
@@ -15,6 +15,7 @@
 class TORRENT_FORM {
 	var $UploadForm = '';
 	var $Categories = array();
+	var $VideoFormats = array();
 	var $Formats = array();
 	var $Bitrates = array();
 	var $Media = array();
@@ -31,11 +32,12 @@ class TORRENT_FORM {
 		$this->Torrent = $Torrent;
 		$this->Error = $Error;
 
-		global $UploadForm, $Categories, $Formats, $Bitrates, $Media, $TorrentID;
+		global $UploadForm, $Categories, $Formats, $VideoFormats, $Bitrates, $Media, $TorrentID;
 
 		$this->UploadForm = $UploadForm;
 		$this->Categories = $Categories;
 		$this->Formats = $Formats;
+		$this->VideoFormats = $VideoFormats
 		$this->Bitrates = $Bitrates;
 		$this->Media = $Media;
 		$this->TorrentID = $TorrentID;
@@ -745,6 +747,71 @@ function show() {
 		TEXTAREA_PREVIEW::JavaScript(false);
 	}//function audiobook_form
 
+		function fullshow_form() {
+		$Torrent = $this->Torrent;
+?>
+		<table cellpadding="3" cellspacing="1" border="0" class="layout border slice" width="100%">
+<?		if ($this->NewTorrent) { ?>
+			<tr id="title_tr">
+				<td class="label">Performance Title:</td>
+				<td>
+					<input type="text" id="title" name="title" size="60" value="<?=display_str($Torrent['Title']) ?>" />
+					<p class="min_padding"></p>
+				</td>
+			</tr>
+<?		} ?>
+			<tr id="year_tr">
+				<td class="label">Year Recorded:</td>
+				<td><input type="text" id="year" name="year" size="5" value="<?=display_str($Torrent['Year']) ?>" /></td>
+			</tr>
+			<tr>
+				<td class="label">Video Codec:</td>
+				<td>
+					<select name="format" onchange="Format()">
+						<option value="">---</option>
+<?
+		foreach (Misc::display_array($this->VideoFormats) as $Format) {
+			echo "\t\t\t\t\t\t<option value=\"$Format\"";
+			if ($Format == $Torrent['Format']) {
+				echo ' selected="selected"';
+			}
+			echo '>';
+			echo $Format;
+			echo "</option>\n";
+		}
+?>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td class="label">Tags:</td>
+				<td>
+					<input type="text" id="tags" name="tags" size="60" value="<?=display_str($Torrent['TagList']) ?>"<? Users::has_autocomplete_enabled('other'); ?> />
+				</td>
+			</tr>
+			<tr>
+				<td class="label">Image (optional):</td>
+				<td><input type="text" id="image" name="image" size="60" value="<?=display_str($Torrent['Image']) ?>"<?=$this->Disabled?> /></td>
+			</tr>
+			<tr>
+				<td class="label">Description:</td>
+				<td>
+<?php new TEXTAREA_PREVIEW('album_desc', 'album_desc', display_str($Torrent['GroupDescription']), 60, 8); ?>
+					<p class="min_padding">Contains information like the track listing, a review, a link to Discogs or MusicBrainz, etc.</p>
+				</td>
+			</tr>
+<?		} ?>
+			<tr>
+				<td class="label">Release description (optional):</td>
+				<td>
+<?php new TEXTAREA_PREVIEW('release_desc', 'release_desc', display_str($Torrent['TorrentDescription']), 60, 8); ?>
+					<p class="min_padding">Contains information like encoder settings. For analog rips, this frequently contains lineage information.</p>
+				</td>
+			</tr>
+		</table>
+<?
+		TEXTAREA_PREVIEW::JavaScript(false);
+	}//function fullshow_form
 
 	function simple_form($CategoryID) {
 		$Torrent = $this->Torrent;
