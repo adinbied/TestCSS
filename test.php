4f8e36e6515b420496fcea81b950f1b715e0886b
adinbied <adinbied@gmail.com>
2017-01-06 22:40:02 -0800

    Update test.php

--- test.php
+++ test.php
@@ -16,12 +16,12 @@
 				<td><input type="text" id="year" name="year" size="5" value="<?=display_str($Torrent['Year']) ?>" /></td>
 			</tr>
 			<tr>
-				<td class="label">Format:</td>
+				<td class="label">Video Codec:</td>
 				<td>
 					<select name="format" onchange="Format()">
 						<option value="">---</option>
 <?
-		foreach (Misc::display_array($this->Formats) as $Format) {
+		foreach (Misc::display_array($this->VideoFormats) as $Format) {
 			echo "\t\t\t\t\t\t<option value=\"$Format\"";
 			if ($Format == $Torrent['Format']) {
 				echo ' selected="selected"';
@@ -34,39 +34,6 @@
 					</select>
 				</td>
 			</tr>
-			<tr>
-				<td class="label">Bitrate:</td>
-				<td>
-					<select id="bitrate" name="bitrate" onchange="Bitrate()">
-						<option value="">---</option>
-<?
-		if (!$Torrent['Bitrate'] || ($Torrent['Bitrate'] && !in_array($Torrent['Bitrate'], $this->Bitrates))) {
-			$OtherBitrate = true;
-			if (substr($Torrent['Bitrate'], strlen($Torrent['Bitrate']) - strlen(' (VBR)')) == ' (VBR)') {
-				$Torrent['Bitrate'] = substr($Torrent['Bitrate'], 0, strlen($Torrent['Bitrate']) - 6);
-				$VBR = true;
-			}
-		} else {
-			$OtherBitrate = false;
-		}
-		foreach (Misc::display_array($this->Bitrates) as $Bitrate) {
-			echo "\t\t\t\t\t\t<option value=\"$Bitrate\"";
-			if ($Bitrate == $Torrent['Bitrate'] || ($OtherBitrate && $Bitrate == 'Other')) {
-				echo ' selected="selected"';
-			}
-			echo '>';
-			echo $Bitrate;
-			echo "</option>\n";
-		}
-?>
-					</select>
-					<span id="other_bitrate_span"<? if (!$OtherBitrate) { echo ' class="hidden"'; } ?>>
-						<input type="text" name="other_bitrate" size="5" id="other_bitrate"<? if ($OtherBitrate) { echo ' value="'.display_str($Torrent['Bitrate']).'"';} ?> onchange="AltBitrate()" />
-						<input type="checkbox" id="vbr" name="vbr"<? if (isset($VBR)) { echo ' checked="checked"'; } ?> /><label for="vbr"> (VBR)</label>
-					</span>
-				</td>
-			</tr>
-<?		if ($this->NewTorrent) { ?>
 			<tr>
 				<td class="label">Tags:</td>
 				<td>
@@ -95,4 +62,4 @@
 		</table>
 <?
 		TEXTAREA_PREVIEW::JavaScript(false);
-	}//function audiobook_form
+	}//function fullshow_form
